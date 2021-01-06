84d85dfb12743c8e9d5476667a677e96bfe5fe75
Anthony N <foto.arn@gmail.com>
2021-01-05 18:31:50 -0800

    + building header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "./App.css";
+import Header from "../src/Header";
 
 function App() {
   return (
     <div className="app">
-      <h1>Linkedin clone.</h1>
       {/* header*/}
+      <Header />
       {/* app body */}
       {/* sidebar */}
       {/* feed */}
