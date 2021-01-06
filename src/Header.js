0425253430eddd8b5fa3065e10c9de4a35d54b81
Anthony N <foto.arn@gmail.com>
2021-01-05 18:49:36 -0800

    + building header

--- src/Header.js
+++ src/Header.js
@@ -1,15 +1,18 @@
 import React from "react";
 import "./Header.css";
+import SearchIcon from "@material-ui/icons/Search";
 
 function Header() {
   return (
     <div className="header">
-      <h1>this is the header</h1>
       <div className="header__left">
-        <img src="" alt="" />
+        <img
+          src="https://www.flaticon.com/svg/static/icons/svg/174/174857.svg"
+          alt="linked-icon"
+        />
 
         <div className="header__search">
-          {/* search icon */}
+          <SearchIcon />
           <input type="text" />
         </div>
       </div>
