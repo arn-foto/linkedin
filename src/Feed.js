31a5159c59707b88577ce2a55e414fee31c75323
Anthony N <foto.arn@gmail.com>
2021-01-06 14:06:05 -0800

    + building feed input

--- src/Feed.js
+++ src/Feed.js
@@ -1,10 +1,18 @@
 import React from "react";
+import CreateIcon from "@material-ui/icons/Create";
+import "./Feed.css";
 
 function Feed() {
   return (
     <div className="feed">
-      <div className="feed__imputContainer">
-        <div className="feed__input"></div>
+      <div className="feed__inputContainer">
+        <div className="feed__input">
+          <CreateIcon />
+          <form>
+            <input type="text" placeholder="Start a post" />
+            <button type="submit">Send</button>
+          </form>
+        </div>
       </div>
     </div>
   );
