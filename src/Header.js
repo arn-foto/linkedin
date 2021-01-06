976acd542e48b4ab22a5447d31f71623d6ba636f
Anthony N <foto.arn@gmail.com>
2021-01-05 19:38:53 -0800

    + building header

--- src/Header.js
+++ src/Header.js
@@ -4,6 +4,9 @@ import SearchIcon from "@material-ui/icons/Search";
 import HeaderOption from "./HeaderOption";
 import HomeIcon from "@material-ui/icons/Home";
 import SupervisorAccountIcon from "@material-ui/icons/SupervisorAccount";
+import BusinessCenterIcon from "@material-ui/icons/BusinessCenter";
+import ChatIcon from "@material-ui/icons/Chat";
+import NotificationsIcon from "@material-ui/icons/Notifications";
 
 function Header() {
   return (
@@ -23,6 +26,13 @@ function Header() {
       <div className="header__right">
         <HeaderOption Icon={HomeIcon} title="Home" />
         <HeaderOption Icon={SupervisorAccountIcon} title="My Network" />
+        <HeaderOption Icon={BusinessCenterIcon} title="Jobs" />
+        <HeaderOption Icon={ChatIcon} title="Messaging" />
+        <HeaderOption Icon={NotificationsIcon} title="Notifications" />
+        <HeaderOption
+          avatar="https://avatars3.githubusercontent.com/u/38259824?s=460&u=f051f7be5c3b35fee8f8d2fdcd667a44da2e51d7&v=4"
+          title="Anthony"
+        />
       </div>
     </div>
   );
