2c12e645d7637b458a0ada779c25eb26d281be11
Anthony N <foto.arn@gmail.com>
2021-01-05 19:18:29 -0800

    + building header

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,9 @@
 import React from "react";
 import "./Header.css";
 import SearchIcon from "@material-ui/icons/Search";
+import HeaderOption from "./HeaderOption";
+import HomeIcon from "@material-ui/icons/Home";
+import SupervisorAccountIcon from "@material-ui/icons/SupervisorAccount";
 
 function Header() {
   return (
@@ -17,7 +20,10 @@ function Header() {
         </div>
       </div>
 
-      <div className="header__right"></div>
+      <div className="header__right">
+        <HeaderOption Icon={HomeIcon} title="Home" />
+        <HeaderOption Icon={SupervisorAccountIcon} title="My Network" />
+      </div>
     </div>
   );
 }
