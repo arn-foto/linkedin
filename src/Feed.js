a1e8a52496cfe15320ca13422bbc317efab0c8eb
Anthony N <foto.arn@gmail.com>
2021-01-06 14:44:36 -0800

    + building feed input

--- src/Feed.js
+++ src/Feed.js
@@ -1,6 +1,11 @@
 import React from "react";
 import CreateIcon from "@material-ui/icons/Create";
+import ImageIcon from "@material-ui/icons/Image";
 import "./Feed.css";
+import InputOption from "../src/InputOption";
+import SubscriptionsIcon from "@material-ui/icons/Subscriptions";
+import EventNoteIcon from "@material-ui/icons/EventNote";
+import CalendarViewDayIcon from "@material-ui/icons/CalendarViewDay";
 
 function Feed() {
   return (
@@ -12,6 +17,16 @@ function Feed() {
             <input type="text" placeholder="Start a post" />
             <button type="submit">Send</button>
           </form>
+        </div>
+        <div className="feed__inputOptions">
+          <InputOption Icon={ImageIcon} title="Photo" color="#70b5f9" />
+          <InputOption Icon={SubscriptionsIcon} title="Video" color="#e7a33e" />
+          <InputOption Icon={EventNoteIcon} title="Event" color="#c0cbcd" />
+          <InputOption
+            Icon={CalendarViewDayIcon}
+            title="Write article"
+            color="#7fc15e"
+          />
         </div>
       </div>
     </div>
