f3e8d006c89211fd442d0b166cc67de60d941786
Anthony N <foto.arn@gmail.com>
2021-01-16 14:04:49 -0800

    + Adding thumbnail

--- src/Header.js
+++ src/Header.js
@@ -12,37 +12,37 @@ import { logout, selectUser } from "./features/userSlice";
 import { auth } from "./firebase";
 
 function Header() {
-  const dispatch = useDispatch();
-  const user = useSelector(selectUser);
+	const dispatch = useDispatch();
+	const user = useSelector(selectUser);
 
-  const logoutOfApp = () => {
-    dispatch(logout());
-    auth.signOut();
-  };
-  return (
-    <div className="header">
-      <div className="header__left">
-        <img
-          src="https://www.flaticon.com/svg/static/icons/svg/174/174857.svg"
-          alt="linked-icon"
-        />
+	const logoutOfApp = () => {
+		dispatch(logout());
+		auth.signOut();
+	};
+	return (
+		<div className="header">
+			<div className="header__left">
+				<img
+					src="https://www.flaticon.com/svg/static/icons/svg/174/174857.svg"
+					alt="linked-icon"
+				/>
 
-        <div className="header__search">
-          <SearchIcon />
-          <input type="text" placeholder="Search" />
-        </div>
-      </div>
+				<div className="header__search">
+					<SearchIcon />
+					<input type="text" placeholder="Search" />
+				</div>
+			</div>
 
-      <div className="header__right">
-        <HeaderOption Icon={HomeIcon} title="Home" />
-        <HeaderOption Icon={SupervisorAccountIcon} title="My Network" />
-        <HeaderOption Icon={BusinessCenterIcon} title="Jobs" />
-        <HeaderOption Icon={ChatIcon} title="Messaging" />
-        <HeaderOption Icon={NotificationsIcon} title="Notifications" />
-        <HeaderOption avatar={user} title="me" onClick={logoutOfApp} />
-      </div>
-    </div>
-  );
+			<div className="header__right">
+				<HeaderOption Icon={HomeIcon} title="Home" />
+				<HeaderOption Icon={SupervisorAccountIcon} title="My Network" />
+				<HeaderOption Icon={BusinessCenterIcon} title="Jobs" />
+				<HeaderOption Icon={ChatIcon} title="Messaging" />
+				<HeaderOption Icon={NotificationsIcon} title="Notifications" />
+				<HeaderOption avatar={user} title="me" onClick={logoutOfApp} />
+			</div>
+		</div>
+	);
 }
 
 export default Header;
